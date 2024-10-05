cb959b0d21dcbaf2b91ebaa3b74d73fa2daef037
Ayan <ayanmulani158@gmail.com>
2024-10-05 10:48:56 +0530

    .

--- src/Pages/Trend/AlbumDynamicGridPage.jsx
+++ src/Pages/Trend/AlbumDynamicGridPage.jsx
@@ -2,11 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { fetchGetDataWithAuth } from '../../Client/Client';
 import { useNavigate } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
-// import BottomHome from '../BottomPage/BottomHome';
-import './trend.css'; // Make sure your CSS is in the right location
-import { Carousel } from 'react-responsive-carousel';
-import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
 
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Spinner from 'react-spinner-material'; 
 
 const AlbumDynamicGridPage = () => {
   const navigate = useNavigate();
@@ -14,7 +13,7 @@ const AlbumDynamicGridPage = () => {
   const [movie, setMovie] = useState([]);
   const [movie2, setMovie2] = useState([]);
   const [movie3, setMovie3] = useState([]);
-
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getData = async () => {
@@ -23,6 +22,8 @@ const AlbumDynamicGridPage = () => {
         setMovie(data);
       } catch (error) {
         console.error(error);
+      } finally {
+        setLoading(false);
       }
     };
     getData();
@@ -32,10 +33,9 @@ const AlbumDynamicGridPage = () => {
     const token = sessionStorage.getItem('token');
     if (token) {
       const decodedToken = jwtDecode(token);
-      if (decodedToken && decodedToken.scope) {
+      if (decodedToken?.scope) {
         const scopes = decodedToken.scope.split(' ');
-        const isAdminRole = scopes.includes('ADMIN');
-        setIsAdmin(isAdminRole);
+        setIsAdmin(scopes.includes('ADMIN'));
       }
     }
   }, []);
@@ -64,107 +64,89 @@ const AlbumDynamicGridPage = () => {
     getData3();
   }, []);
 
-
   const handleImageClick = (data) => {
-    console.log("Movie clicked:", data);
     navigate('/detail', { state: { movie: data, previousPage: 'album' } });
   };
 
-  const displayAdd = () => {
-    navigate('/add/Trend');
+  const displayAdd = (type) => {
+    navigate(`/add/${type}`);
   };
 
-  const displayAdd2 = () => {
-    navigate('/add/Most');
-  };
-
-  const displayAdd3 = () => {
-    navigate('/add/Top');
-  };
-
-  const MovieSection = React.memo(({ title, movies,onClickAdd }) => (
-      <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%', paddingRight: 60 }}>
-        <p style={{ justifyContent: 'start', marginLeft: 60, fontSize: 30, fontWeight: 'normal' }}>{title}</p>
-        {isAdmin && <button className='addButton' onClick={onClickAdd}>Add</button>}
-    
-    
-        <Carousel
-        className={styles.carousel}
-        autoPlay={false}           // Automatically play the slides
-        showStatus={false}        // Hide the status indicator
-        dynamicHeight={true}     // Fix the height of the carousel
-        axis='horizontal'         // Set the carousel to slide horizontally
-        infiniteLoop={false}       // Loop the slides infinitely
-        stopOnHover={false}       // Continue auto-playing even on hover
-        showThumbs={false}        
-        >
-          <div>
-          {movies && movies.map((data, index) => (
-            <div key={index} className="movie-item">
-              <img
-                src={data.photourl1}
-                alt={data.name}
-                onClick={() => handleImageClick(data)}
-                style={{ cursor: 'pointer' }}
-                loading="lazy"
-              />
-            </div>
-            
-          ))}
+  const MovieSection = ({ title, movies }) => (
+    <div className="movie-section">
+      <div className="section-header">
+        <p className="section-title">{title}</p>
+        {isAdmin && (
+          <button className="add-button" onClick={() => displayAdd(title)}>
+            Add
+          </button>
+        )}
+      </div>
+      <Carousel
+        className="movie-carousel"
+        autoPlay={false}
+        showStatus={false}
+        dynamicHeight={true}
+        axis="horizontal"
+        infiniteLoop={false}
+        stopOnHover={true}
+        showThumbs={false}
+        emulateTouch={true}
+      >
+        {movies.map((data, index) => (
+          <div key={index} className="movie-item">
+            <img
+              src={data.photourl1}
+              alt={data.name}
+              onClick={() => handleImageClick(data)}
+              className="movie-image"
+              loading="lazy"
+            />
           </div>
-          </Carousel>
-     
-     </div>
-  
-  ));
+        ))}
+      </Carousel>
+    </div>
+  );
 
   return (
-    <div>
-     
-<Carousel 
-className={styles.carousel}
-  autoPlay={true}           // Automatically play the slides
-  showStatus={false}        // Hide the status indicator
-  dynamicHeight={true}     // Fix the height of the carousel
-  axis='horizontal'         // Set the carousel to slide horizontally
-  infiniteLoop={true}       // Loop the slides infinitely
-  interval={3000}           // Interval between slides (in milliseconds)
-  stopOnHover={false}       // Continue auto-playing even on hover
-  showThumbs={false}        // Hide thumbnail previews
-
->
-          <div>
-          <img src="https://occ-0-2087-2186.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABSnpnCU2Bb-QjJmTVcTU6gG57sYTE-q2UOx2GsLEjFX83tNvgxB5yFtpqyJQGAAB21o_O9VYKdOKPfxe7joIcWaMPhrF3YRfNNBP.jpg?r=afe" alt="" />
-          </div>
-          <div>
-          <img src="https://occ-0-2087-2186.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABaYQ96l7chKogWW9hfK9XGBbJWrb7aGlNeZwGt9uDhKRUaNxYA6g9ydeEVJEP4UWYqqZpeh5ja0N9tOWXdtJFy1Bj0wbaE8ginb3.jpg?r=eac&quot;" alt="Red notice" />
-          </div>
-         <div>
-          <img src="https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABUq0aH_83uPoyI-XUOTJeYABGdOHPRd6dFTEjBQWYc8UTYM9RNTyiu9TDrBtxHtTdVrEB3DdovNp3tBNch0NcXBHEMjmYR97K4sK.jpg?r=e44" alt="Red notice" />
-          </div>
-           <div>
-          <img src="https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABcaZlB5cBXaQovKzRNe3DLOD5xe5ug2Msp7y4SHAFXs8Uu-s9esOCD1X3jnbYZZ4Dm-tM-cOgWh1FDmFD0wIfIfbrkpJAoULvXWX.jpg?r=c9b" alt="" />
-          </div>
-        </Carousel>
-
-        <Carousel >
-          <div>
-
-        <MovieSection title="Most Watched" movies={movie} onClickAdd={displayAdd}/>
-
+    <div className="album-dynamic-grid-page">
+      {loading ? (
+        <div className="loading-spinner">
+          <Spinner size={120} spinnerColor={"#333"} spinnerWidth={2} visible={true} />
         </div>
-        </Carousel>
-        <div>
+      ) : (
+        <>
+          <Carousel
+            autoPlay={true}
+            showStatus={false}
+            dynamicHeight={true}
+            axis="horizontal"
+            infiniteLoop={true}
+            interval={3000}
+            stopOnHover={true}
+            showThumbs={false}
+            emulateTouch={true}
+          >
+            <div>
+              <img src="https://occ-0-2087-2186.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABSnpnCU2Bb-QjJmTVcTU6gG57sYTE-q2UOx2GsLEjFX83tNvgxB5yFtpqyJQGAAB21o_O9VYKdOKPfxe7joIcWaMPhrF3YRfNNBP.jpg?r=afe" alt="" />
+            </div>
+            <div>
+              <img src="https://occ-0-2087-2186.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABaYQ96l7chKogWW9hfK9XGBbJWrb7aGlNeZwGt9uDhKRUaNxYA6g9ydeEVJEP4UWYqqZpeh5ja0N9tOWXdtJFy1Bj0wbaE8ginb3.jpg?r=eac" alt="Red notice" />
+            </div>
+            <div>
+              <img src="https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABUq0aH_83uPoyI-XUOTJeYABGdOHPRd6dFTEjBQWYc8UTYM9RNTyiu9TDrBtxHtTdVrEB3DdovNp3tBNch0NcXBHEMjmYR97K4sK.jpg?r=e44" alt="Red notice" />
+            </div>
+            <div>
+              <img src="https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABcaZlB5cBXaQovKzRNe3DLOD5xe5ug2Msp7y4SHAFXs8Uu-s9esOCD1X3jnbYZZ4Dm-tM-cOgWh1FDmFD0wIfIfbrkpJAoULvXWX.jpg?r=c9b" alt="" />
+            </div>
+          </Carousel>
 
-        <MovieSection title="Sci-Fi Dramas" movies={movie2} onClickAdd={displayAdd2} />
-        </div>
-        <Carousel >
-        <div>
-        <MovieSection title="Family Movies" movies={movie3} onClickAdd={displayAdd3} />
-        </div>
-        {/* <BottomHome /> */}
-      </Carousel>
-      </div>
+          <MovieSection title="Most Watched" movies={movie} />
+          <MovieSection title="Sci-Fi Dramas" movies={movie2} />
+          <MovieSection title="Family Movies" movies={movie3} />
+        </>
+      )}
+    </div>
   );
 };
 
