d23223ff4acc8da0e03d3325619249063d762e2a
Ayan <ayanmulani158@gmail.com>
2024-10-05 10:31:43 +0530

    some changes in code

--- src/Pages/Trend/AlbumDynamicGridPage.jsx
+++ src/Pages/Trend/AlbumDynamicGridPage.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchGetDataWithAuth } from '../../Client/Client';
 import { useNavigate } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
-import BottomHome from '../BottomPage/BottomHome';
+// import BottomHome from '../BottomPage/BottomHome';
 import './trend.css'; // Make sure your CSS is in the right location
+import { Carousel } from 'react-responsive-carousel';
+import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 
 const AlbumDynamicGridPage = () => {
@@ -12,10 +14,7 @@ const AlbumDynamicGridPage = () => {
   const [movie, setMovie] = useState([]);
   const [movie2, setMovie2] = useState([]);
   const [movie3, setMovie3] = useState([]);
-  const container = useRef(null);
-  const movieContainerRef1 = useRef(null);
-  const movieContainerRef2 = useRef(null);
-  const movieContainerRef3 = useRef(null);
+
 
   useEffect(() => {
     const getData = async () => {
@@ -65,27 +64,6 @@ const AlbumDynamicGridPage = () => {
     getData3();
   }, []);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (container.current) {
-        scrollRight(container);
-      }
-    }, 3000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  const scrollLeft = (containerRef) => {
-    if (containerRef.current) {
-      containerRef.current.scrollBy({ left: -window.innerWidth, behavior: 'smooth' });
-    }
-  };
-
-  const scrollRight = (containerRef) => {
-    if (containerRef.current) {
-      containerRef.current.scrollBy({ left: window.innerWidth, behavior: 'smooth' });
-    }
-  };
 
   const handleImageClick = (data) => {
     console.log("Movie clicked:", data);
@@ -104,15 +82,13 @@ const AlbumDynamicGridPage = () => {
     navigate('/add/Top');
   };
 
-  const MovieSection = React.memo(({ title, movies, containerRef, onClickAdd }) => (
-    <div className="album-dynamic-grid">
+  const MovieSection = React.memo(({ title, movies,onClickAdd }) => (
       <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%', paddingRight: 60 }}>
         <p style={{ justifyContent: 'start', marginLeft: 60, fontSize: 30, fontWeight: 'normal' }}>{title}</p>
         {isAdmin && <button className='addButton' onClick={onClickAdd}>Add</button>}
-      </div>
-      <div className="scroll-buttons">
-        <button onClick={() => scrollLeft(containerRef)} className="scroll-btn left-btn">&lt;</button>
-        <div className="movie-container" ref={containerRef}>
+    
+    
+        <carousel>
           {movies && movies.map((data, index) => (
             <div key={index} className="movie-item">
               <img
@@ -124,29 +100,59 @@ const AlbumDynamicGridPage = () => {
               />
             </div>
           ))}
-        </div>
-        <button onClick={() => scrollRight(containerRef)} className="scroll-btn right-btn">&gt;</button>
-      </div>
-    </div>
+          </carousel>
+     
+     </div>
+  
   ));
 
   return (
     <div>
-      <div className="top-image-scroll-container" ref={container}>
-        <div className="image-wrapper">
+     
+<Carousel 
+className={styles.carousel}
+  autoPlay={true}           // Automatically play the slides
+  showStatus={false}        // Hide the status indicator
+  dynamicHeight={true}     // Fix the height of the carousel
+  axis='horizontal'         // Set the carousel to slide horizontally
+  infiniteLoop={true}       // Loop the slides infinitely
+  interval={3000}           // Interval between slides (in milliseconds)
+  stopOnHover={false}       // Continue auto-playing even on hover
+  showThumbs={false}        // Hide thumbnail previews
+
+>
+          <div>
           <img src="https://occ-0-2087-2186.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABSnpnCU2Bb-QjJmTVcTU6gG57sYTE-q2UOx2GsLEjFX83tNvgxB5yFtpqyJQGAAB21o_O9VYKdOKPfxe7joIcWaMPhrF3YRfNNBP.jpg?r=afe" alt="" />
+          </div>
+          <div>
           <img src="https://occ-0-2087-2186.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABaYQ96l7chKogWW9hfK9XGBbJWrb7aGlNeZwGt9uDhKRUaNxYA6g9ydeEVJEP4UWYqqZpeh5ja0N9tOWXdtJFy1Bj0wbaE8ginb3.jpg?r=eac&quot;" alt="Red notice" />
+          </div>
+         <div>
           <img src="https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABUq0aH_83uPoyI-XUOTJeYABGdOHPRd6dFTEjBQWYc8UTYM9RNTyiu9TDrBtxHtTdVrEB3DdovNp3tBNch0NcXBHEMjmYR97K4sK.jpg?r=e44" alt="Red notice" />
+          </div>
+           <div>
           <img src="https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABcaZlB5cBXaQovKzRNe3DLOD5xe5ug2Msp7y4SHAFXs8Uu-s9esOCD1X3jnbYZZ4Dm-tM-cOgWh1FDmFD0wIfIfbrkpJAoULvXWX.jpg?r=c9b" alt="" />
+          </div>
+        </Carousel>
+
+        <Carousel >
+          <div>
+
+        <MovieSection title="Most Watched" movies={movie} onClickAdd={displayAdd}/>
+
         </div>
+        </Carousel>
+        <div>
+
+        <MovieSection title="Sci-Fi Dramas" movies={movie2} onClickAdd={displayAdd2} />
+        </div>
+        <Carousel >
+        <div>
+        <MovieSection title="Family Movies" movies={movie3} onClickAdd={displayAdd3} />
+        </div>
+        {/* <BottomHome /> */}
+      </Carousel>
       </div>
-      <div className='shows-section'>
-        <MovieSection title="Most Watched" movies={movie} onClickAdd={displayAdd} containerRef={movieContainerRef1} />
-        <MovieSection title="Sci-Fi Dramas" movies={movie2} onClickAdd={displayAdd2} containerRef={movieContainerRef2} />
-        <MovieSection title="Family Movies" movies={movie3} onClickAdd={displayAdd3} containerRef={movieContainerRef3} />
-        <BottomHome />
-      </div>
-    </div>
   );
 };
 
